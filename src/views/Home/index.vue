9b92045273473c4fbd7e59b0051bee360a631e6f
demigods <a13752365641@126.com>
2021-03-31 16:57:41 +0800

    创建示例组件。

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div>Home</div>
+  <DgDemo />
 </template>
 
 <script>
+import DgDemo from '_c/DgDemo/DgDemo.vue'
 export default {
   name: 'Home',
+  components: {
+    DgDemo
+  },
   props: {},
   setup () {
 
