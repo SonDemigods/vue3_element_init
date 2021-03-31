3f8b0e633c8c7e7b6be9c2f4c42c40471d9a7eee
demigods <a13752365641@126.com>
2021-03-31 09:12:06 +0800

    添加编译路径的别名。

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    // 路径别名
+    alias: {
+      // 项目根路径
+      '@': '/src',
+      // 组件路径
+      '_c': '/src/components',
+      // 资源路径
+      '_a': '/src/assets'
+    }
+  },
   plugins: [vue()]
 })
