ab17e8081984a574c030bf83cac1b811e296e98c
demigods <a13752365641@126.com>
2021-04-02 13:16:43 +0800

    添加默认布局。

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
-  <img alt="Vue logo"
-       src="./assets/logo.png" />
-  <router-view />
+  <Layout />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import Layout from '@/layout/index.vue'
+
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {
+    Layout,
+  },
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
 </style>
---
+++ src/layout/Sidebar/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Sidebar</div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  components: {},
+  props: {},
+  setup () {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
