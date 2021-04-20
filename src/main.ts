9622ec520d31624bd18421f93df1d9fac83e7cb4
demigods <a13752365641@126.com>
2021-04-20 14:42:45 +0800

    修改css引入位置。

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 // 导入ui组件
 import ElementPlus from 'element-plus'
 // 导入ui组件的样式文件
-import 'element-plus/packages/theme-chalk/src/base.scss'
+import 'element-plus/lib/theme-chalk/index.css'
 
 // 导入根组件
 import App from './App.vue'
