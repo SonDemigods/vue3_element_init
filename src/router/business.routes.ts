705faf9f14f5449cbe4a40db23db96d980c3402b
demigods <a13752365641@126.com>
2021-04-25 17:45:31 +0800

    分组路由默认使用main组件。

--- src/router/business.routes.ts
+++ src/router/business.routes.ts
@@ -7,23 +7,23 @@ export default [{
   },
   component: () => import(/* webpackChunkName: "Home" */ '@/views/System/Home/index.vue')
 }, {
-  path: '/PageDemo',
-  name: 'PageDemo',
+  path: '/Demo',
+  name: 'Demo',
   meta: {
     title: '示例页面',
     menuHide: false
   },
+  component: () => import(/* webpackChunkName: "Main" */ '@/layout/Main/index.vue'),
   children: [
     {
-      path: 'Home1',
-      name: 'Home1',
+      path: '/PageDemo',
+      name: 'PageDemo',
       meta: {
         title: '子页面',
         icon: 's-grid',
         menuHide: false
       },
-      component: () => import(/* webpackChunkName: "Home" */ '@/views/System/Home/index.vue')
+      component: () => import(/* webpackChunkName: "PageDemo" */ '@/views/Business/PageDemo/index.vue')
     },
-  ],
-  component: () => import(/* webpackChunkName: "PageDemo" */ '@/views/Business/PageDemo/index.vue')
+  ]
 }]
