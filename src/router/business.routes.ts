516d468c709108334848c2583362d3da50e88248
demigods <a13752365641@126.com>
2022-02-17 16:41:48 +0800

    feat:添加示例路由。

--- src/router/business.routes.ts
+++ src/router/business.routes.ts
@@ -26,4 +26,24 @@ export default [{
       component: () => import(/* webpackChunkName: "PageDemo" */ '@/views/Business/PageDemo/index.vue')
     },
   ]
+}, {
+  path: '/Demo2',
+  name: 'Demo2',
+  meta: {
+    title: '示例页面2',
+    menuHide: false
+  },
+  component: () => import(/* webpackChunkName: "Main" */ '@/layout/Main/index.vue'),
+  children: [
+    {
+      path: '/PageDemo2',
+      name: 'PageDemo2',
+      meta: {
+        title: '子页面',
+        icon: 's-grid',
+        menuHide: false
+      },
+      component: () => import(/* webpackChunkName: "PageDemo2" */ '@/views/Business/PageDemo/index.vue')
+    },
+  ]
 }]
